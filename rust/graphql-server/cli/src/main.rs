670a222b9c046e84231d821c626340869d7b75a4
univan0928 <82901836+univan0928@users.noreply.github.com>
2021-11-02 22:35:22 -0400

    Initialize feat/rust-graphql-api

---
+++ rust/graphql-server/cli/src/main.rs
@@ -0,0 +1,20 @@
+use std::thread;
+
+use spl_graphql_server::schema::{create_schema, Ctx};
+use spl_graphql_server::server::AppServer;
+
+#[tokio::main]
+async fn main() {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
+    let context = Ctx::new();
+    let mut ctx = Ctx::clone(&context);
+
+    let server = AppServer::new(create_schema, context);
+    thread::spawn(move || {
+      ctx.preload();
+    });
+
+    server.run().await
+}
