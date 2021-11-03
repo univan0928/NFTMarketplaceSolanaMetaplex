2466bdce696b217fdc3b3aa8f00115d7f4c1c74d
univan0928 <82901836+univan0928@users.noreply.github.com>
2021-11-02 22:34:46 -0400

    Initialize custom-auction-text

---
+++ js/packages/web/src/components/ArtContent/index.tsx
@@ -0,0 +1,113 @@
+import React, { Ref, useState } from 'react';
+import { Image } from 'antd';
+import { MetadataCategory } from '@oyster/common';
+import { MeshViewer } from '../MeshViewer';
+import { ThreeDots } from '../MyLoader';
+import { useCachedImage } from '../../hooks';
+import { Stream, StreamPlayerApi } from '@cloudflare/stream-react';
+
+const HACK_LOOKUP: Record<string, string> = {
+  'https://arweave.net/DZQLWAoc6MhpGoooFMrFF5okwUUwp034mtyrkzdFGfg':
+  '9a800738aac474388b998c04f8775ad6',
+  'https://arweave.net/BPJbVgBUCwDLALLruex_5cmXvffXvcCwwweoiJ45BUM':
+    '01c2892dc406033a0dfbaf4ab883e448',
+  'https://arweave.net/__2eOiYv0w-2_ayLUxqiSBQZeC9z5qcJPbeSE657Dcw':
+    '0af8af868293c5a6b6aad8ea2f9b985c',
+  'https://arweave.net/0PiQ1Iybbp07nLXXj2w2OzmE2BA9VAxu_sBPUkfjCX8':
+    'bd9fbed75d9d58dbd9520cdbb7a407ae',
+  'https://arweave.net/yftUPSwuKEyfazIi_vfKCSE-JrghtDDKfTbq0d-dmJ4':
+    'acd90bc0d6bd75565eca507fb69c7263',
+  'https://arweave.net/9Dd_JTurpzTPiz1prvNpS-PexkahCLTeXLVXUIT0qbE':
+    'd90b177c2f77eab0fd70dec688d12d72',
+  'https://arweave.net/WNLzR36v80IS61yeWyPYstF3qacWDXcDmYLp-RldYFQ':
+    '0cb4b80b5a668072c9f3a0a4224628f6',
+  'https://arweave.net/PHEyKMsLA0AfjLt0UyGyOa9NBSgJuKT2wHxcPca-Qs8':
+    'a2a5deeafed881d67aed547f7ffaa03d',
+  'https://arweave.net/5A8KJmRh2qYBNFdO0ChgJ_0Jx0ZgOFSatU2ffJg4SrA':
+    '3e5eadb976fab8fe7330a760941a960f',
+};
+
+export const ArtContent = ({
+  uri,
+  extension,
+  category,
+  className,
+  preview,
+  style,
+  files,
+  width,
+  height,
+  ref,
+}: {
+  category?: MetadataCategory;
+  extension?: string;
+  uri?: string;
+  className?: string;
+  preview?: boolean;
+  style?: React.CSSProperties;
+  width?: number;
+  height?: number;
+  files?: string[];
+  ref?: Ref<HTMLDivElement>;
+}) => {
+  const [loaded, setLoaded] = useState<boolean>(false);
+  const playerApiRef = React.useRef<StreamPlayerApi>(null);
+  const src = useCachedImage(uri || '');
+
+  if (extension?.endsWith('.glb') || category === 'vr') {
+    return <MeshViewer url={uri} className={className} style={style} />;
+  }
+  const likelyVideo = (files || []).filter((f, index, arr) => {
+    // TODO: filter by fileType
+    return arr.length >= 2 ? index === 1 : index === 0;
+  })[0];
+
+  return category === 'video' ? (
+    HACK_LOOKUP[likelyVideo] ? (
+      <div className={`${className} square`}>
+        <Stream
+          streamRef={playerApiRef}
+          src={HACK_LOOKUP[likelyVideo]}
+          muted={true}
+          loop={true}
+          height={600}
+          width={600}
+          videoDimensions={{
+            videoHeight: 600,
+            videoWidth: 600,
+          }}
+          controls={false}
+          // responsive={true}
+          autoplay={true}
+        />
+      </div>
+    ) : (
+      <video
+        className={className}
+        playsInline={true}
+        autoPlay={false}
+        muted={true}
+        controls={true}
+        controlsList="nodownload"
+        style={style}
+        loop={true}
+        poster={extension}
+      >
+        <source src={likelyVideo} type="video/mp4" style={style} />
+      </video>
+    )
+  ) : (
+    <Image
+      src={src}
+      preview={preview}
+      wrapperClassName={className}
+      loading="lazy"
+      wrapperStyle={{ ...style }}
+      onLoad={e => {
+        setLoaded(true);
+      }}
+      placeholder={<ThreeDots />}
+      {...(loaded ? {} : { height: 200 })}
+    />
+  );
+};
---
+++ js/packages/web/src/views/home/index.tsx
@@ -0,0 +1,121 @@
+import React, { useState, useMemo } from 'react';
+import { Layout, Row, Col, Tabs } from 'antd';
+import Masonry from 'react-masonry-css';
+
+import { PreSaleBanner } from '../../components/PreSaleBanner';
+import { AuctionViewState, useAuctions } from '../../hooks';
+
+import './index.less';
+import { AuctionRenderCard } from '../../components/AuctionRenderCard';
+import { Link } from 'react-router-dom';
+import { CardLoader } from '../../components/MyLoader';
+import { useMeta } from '../../contexts';
+import BN from 'bn.js';
+
+const { TabPane } = Tabs;
+
+const { Content } = Layout;
+export const HomeView = () => {
+  const auctions = useAuctions(AuctionViewState.Live);
+  const auctionsEnded = useAuctions(AuctionViewState.Ended);
+  const { isLoading } = useMeta();
+  const breakpointColumnsObj = {
+    default: 4,
+    1100: 3,
+    700: 2,
+    500: 1,
+  };
+
+  const heroAuction = useMemo(
+    () =>
+      auctions.filter(a => {
+        // const now = moment().unix();
+        return !a.auction.info.ended();
+        // filter out auction for banner that are further than 30 days in the future
+        // return Math.floor(delta / 86400) <= 30;
+      })?.[0],
+    [auctions],
+  );
+
+  const liveAuctions = auctions
+  .sort((a, b) => a.auction.info.endedAt?.sub(b.auction.info.endedAt || new BN(0)).toNumber() || 0)
+  .filter((m, idx) => idx < 10);
+
+  const liveAuctionsView = (
+    <Masonry
+      breakpointCols={breakpointColumnsObj}
+      className="my-masonry-grid"
+      columnClassName="my-masonry-grid_column"
+    >
+      {!isLoading
+        ? liveAuctions.map((m, idx) => {
+              if (m === heroAuction) {
+                return;
+              }
+
+              const id = m.auction.pubkey.toBase58();
+              return (
+                <Link to={`/auction/${id}`} key={idx}>
+                  <AuctionRenderCard key={id} auctionView={m} />
+                </Link>
+              );
+            })
+        : [...Array(10)].map((_, idx) => <CardLoader key={idx} />)}
+    </Masonry>
+  );
+  const endedAuctions = (
+    <Masonry
+      breakpointCols={breakpointColumnsObj}
+      className="my-masonry-grid"
+      columnClassName="my-masonry-grid_column"
+    >
+      {!isLoading
+        ? auctionsEnded
+            .filter((m, idx) => idx < 10)
+            .map((m, idx) => {
+              if (m === heroAuction) {
+                return;
+              }
+
+              const id = m.auction.pubkey.toBase58();
+              return (
+                <Link to={`/auction/${id}`} key={idx}>
+                  <AuctionRenderCard key={id} auctionView={m} />
+                </Link>
+              );
+            })
+        : [...Array(10)].map((_, idx) => <CardLoader key={idx} />)}
+    </Masonry>
+  );
+
+  return (
+    <Layout style={{ margin: 0, marginTop: 30 }}>
+      <PreSaleBanner auction={heroAuction} />
+      <Layout>
+        <Content style={{ display: 'flex', flexWrap: 'wrap' }}>
+          <Col style={{ width: '100%', marginTop: 10 }}>
+            {liveAuctions.length > 0 && (<Row>
+              <Tabs>
+                <TabPane>
+                  <h2>Live Auctions</h2>
+                  {liveAuctionsView}
+                </TabPane>
+              </Tabs>
+            </Row>)}
+            <Row>
+              {auctionsEnded.length > 0 && (
+              <Tabs>
+                <TabPane>
+                  <h2>Ended Auctions</h2>
+                  {endedAuctions}
+                </TabPane>
+              </Tabs>
+              )}
+              <br />
+            </Row>
+          </Col>
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
